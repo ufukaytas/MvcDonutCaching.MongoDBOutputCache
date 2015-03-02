a4db6b0c484d0c217d558d116e95ddd42c3bcf66
Sergio León <panicoenlaxbox@gmail.com>
2015-03-02 12:48:49 +0100

    Preparación para publicación de paquete Nuget vía .nuspec

--- MongoDBOutputCache/Properties/AssemblyInfo.cs
+++ MongoDBOutputCache/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MongoDBOutputCache")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("MvcDonutCaching.MongoDBOutputCache")]
+[assembly: AssemblyDescription("ASP.NET MVC OutputCache MongoDB for MvcDonutCaching")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MongoDBOutputCache")]
+[assembly: AssemblyCompany("panicoenlaxbox")]
+[assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
